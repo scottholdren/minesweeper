411ebe099e98f16318ab7c49f7a86edb5e8ec538
scottholdren <scott@holdren.com>
2023-06-01 10:30:23 -0400

    these columns are automatically created

--- db/migrate/20230601142335_create_boards.rb
+++ db/migrate/20230601142335_create_boards.rb
@@ -1,8 +1,6 @@
 class CreateBoards < ActiveRecord::Migration[7.0]
   def change
     create_table :boards do |t|
-      t.primary_key :id
-      t.timestamp :created_at
       t.string :email
       t.string :name
       t.integer :height
